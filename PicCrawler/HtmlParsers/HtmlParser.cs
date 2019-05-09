b5ebed3c73aceab792a80e8631efa632798331e7
VennyCooper <wenqiangluo1010@outlook.com>
2019-05-09 21:47:56 +0800

    Add project files.

---
+++ PicCrawler/HtmlParsers/HtmlParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicCrawler.HtmlParsers
+{
+    public abstract class HtmlParser
+    {
+        protected string RootUri { get; set; }
+
+        protected abstract 
+    }
+}
